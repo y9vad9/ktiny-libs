2ee17fd041bb1e4ac878a11d302c5d3675374ee2
Vadym Yaroshchuk <y9vad9@gmail.com>
2025-01-04 02:20:43 +0100

    fix: missing publication info

--- kotlidator/build.gradle.kts
+++ kotlidator/build.gradle.kts
@@ -1,3 +1,16 @@
 plugins {
     id(libs.plugins.conventions.multiplatform.library.get().pluginId)
 }
+
+mavenPublishing {
+    coordinates(
+        groupId = "com.y9vad9.ktiny",
+        artifactId = "kotlidator",
+        version = System.getenv("LIB_VERSION") ?: return@mavenPublishing,
+    )
+
+    pom {
+        name.set("Kotlin Validation Library")
+        description.set("Kotlin validation library with type-safe results.")
+    }
+}
